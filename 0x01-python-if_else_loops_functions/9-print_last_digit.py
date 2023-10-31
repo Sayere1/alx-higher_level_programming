9ddabce81c927381126df1a8f27139f59d6b9a69
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-10-31 07:44:54 -0700

    3 colors, 10 digits, and 7 notes

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def print_last_digit(x):
     if x > 0:
-        print (x % 10, end="")
+        print(x % 10, end="")
         return (x % 10)
     elif x == 0:
         print(0, end="")
