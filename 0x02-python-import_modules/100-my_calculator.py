e66c99e78a79eaeca3f224dbb596b0cfa3252f76
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-11-02 08:10:28 -0700

    This program import all functions and nd handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,26 +2,19 @@
 
 if __name__ == "__main__":
 
+    from calculator_1 import add, sub, mul, div
     import sys
-    from calculator_1 import add, mul, sub, div
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    signs = ["*", "+", "-", "/"]
-    argv = sys.argv
-    m = argv[1]
-    n = argv[2]
-    o = argv[3]
-    if y not in signs:
+
+    basic_op = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(basic_op.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    else:
-        if argv[2] == '+':
-            print("{} {} {} = {}".format(m, n, o, add(int(m), int(o))))
-        elif argv[2] == "*":
-            print("{} {} {} = {}".format(m, n, o, mul(int(m), int(o))))
-        elif argv[2] == '/':
-            print("{} {} {} = {}".format(m, n, o, div(int(m), int(o))))
-        elif argv[2] == '-':
-            print("{} {} {} = {}".format(m, n, o, sub(int(m), int(o))))
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, basic_op[sys.argv[2]](a, b)))
