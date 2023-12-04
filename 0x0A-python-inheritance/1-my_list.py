b450d9cd540b56296597448026e12a237377edfc
Sayere1 <osayemwenreosaigbovo@gmail.com>
2023-12-04 06:25:55 -0800

    tttt

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""Define an inherited list class MyList"""
+"""
+contains the MyList class
+"""
+
 
 class MyList(list):
-    """Type class MyList with print_sorted function"""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
+        """prints the sorted list"""
         print(sorted(self))
