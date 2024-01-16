088fb14ea822d7474faae4d7c5658a43c709f02f
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-01-16 10:22:23 -0800

    insert, display and create mul row table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- new row in first_table is inserted in my MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Displays the num of records with id = 89 in first_table in my MySQL server.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
