3cc25f44487d37f26144dc81e8aeceaa638d032d
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-02-17 03:42:03 -0800

    Get a state % Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,8 +12,8 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1],
-                               sys.argv[2],
-                               sys.argv[3]))
+                            sys.argv[2],
+                            sys.argv[3]))
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
