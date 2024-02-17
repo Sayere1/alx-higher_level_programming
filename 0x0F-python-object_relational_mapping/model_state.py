1d1ddbfccc6bd74eb7e82bc2f813ad620b196124
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-02-17 03:01:18 -0800

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,7 @@ mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
-    """ Class with id and name attributes representing each state"""
+    """  Class with id and name attributes representing each state """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
