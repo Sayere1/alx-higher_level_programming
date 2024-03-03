a574dab7d46bc27dc775807c8a4fb8e652ed3669
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-03 05:32:35 -0800

    cURL only method and header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument and displays body of response
+# script that takes in a URL as an argument and displays body o
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
