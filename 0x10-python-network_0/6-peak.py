66d8751f41a44e074b3f429d8b495fd1630a36d2
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-03 05:40:43 -0800

    Parameter and find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function that finds a peak in a list of unsorted integers.
+You are not allowed to import any module
+"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
