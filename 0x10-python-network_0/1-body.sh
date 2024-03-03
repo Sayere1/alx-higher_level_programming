dc996743a3e2846576f3aa3b86af6ee2a3d3a845
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-03 03:07:06 -0800

    cURL body size, end and method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL
-# and displays the body of the response
+# Bscript that sends a GET request to the URL and displays body of response
 curl -sL "$1"
