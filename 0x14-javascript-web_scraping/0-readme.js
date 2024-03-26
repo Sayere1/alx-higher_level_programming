ee74347b40d938cb8b3154e8d23a8429545d3aef
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-26 07:14:43 -0700

    README

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
+// script that reads and prints the content of a file
 
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readFile(file, 'utf-8',
-	function (err, data) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(data);
-		}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
