9beb0c5a78c2bc24390039838a8c968a2d5ea5dd
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-26 07:26:17 -0700

    write n code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,6 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2],
-	process.argv[3],
-	error => {
-		if (error) console.log(error);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
