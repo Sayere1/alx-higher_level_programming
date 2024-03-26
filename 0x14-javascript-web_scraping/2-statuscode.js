fcc6df3d4b639db5299b9cbd4a8275d2963ae676
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-26 07:32:16 -0700

    write n code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,8 @@
 // script that display the status code of a GET request.
 
 const request = require('request');
-const url = process.argv[2];
 
 request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
-});
+}
+);
